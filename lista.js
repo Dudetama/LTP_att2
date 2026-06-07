d74746cb66289c5441b6000503edcbdb477422d1
Dudetama <motaeduarda141@gmail.com>
2026-06-07 17:23:29 -0400

    exercicio 6

--- lista.js
+++ lista.js
@@ -124,3 +124,41 @@ const usuario = new Usuario("Julia", "1234");
 console.log(usuario.autenticar("1234"));
 usuario.alterarSenha("1234", "abcd");
 console.log(usuario.autenticar("abcd"));
+
+class Pilha {
+    #itens;
+
+    constructor() {
+        this.#itens = [];
+    }
+
+    empilhar(item) {
+        this.#itens.push(item);
+    }
+
+    desempilhar() {
+        return this.#itens.pop();
+    }
+
+    topo() {
+        return this.#itens[this.#itens.length - 1];
+    }
+
+    estaVazia() {
+        return this.#itens.length == 0;
+    }
+
+    tamanho() {
+        return this.#itens.length;
+    }
+}
+
+const pilha = new Pilha();
+pilha.empilhar(10);
+pilha.empilhar(20);
+pilha.empilhar(30);
+console.log(pilha.topo());
+console.log(pilha.tamanho());
+pilha.desempilhar();
+console.log(pilha.topo());
+console.log(pilha.estaVazia());
