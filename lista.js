8f60b5b9df958171e9de3d04dcc56dd8529a9f10
Dudetama <motaeduarda141@gmail.com>
2026-06-07 17:17:26 -0400

    exercicio 3

--- lista.js
+++ lista.js
@@ -68,3 +68,32 @@ const p2 = new Produto("Teclado", 100, 5);
 
 console.log(p1.calcularValorEstoque());
 console.log(Produto.obterTotal());
+
+class Temperatura {
+    #celsius;
+
+    constructor(celsius) {
+        this.#celsius = celsius;
+    }
+
+    get celsius() {
+        return this.#celsius;
+    }
+
+    set celsius(valor) {
+        this.#celsius = valor;
+    }
+
+    get fahrenheit() {
+        return (this.#celsius * 9/5) + 32;
+    }
+
+    get kelvin() {
+        return this.#celsius + 273.15;
+    }
+}
+
+const temp = new Temperatura(25);
+console.log(`Celsius: ${temp.celsius}`);
+console.log(`Fahrenheit: ${temp.fahrenheit}`);
+console.log(`Kelvin: ${temp.kelvin}`);
