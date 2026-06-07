762a37d7352ddf49b38643dfe3d714b6b0209eb0
Dudetama <motaeduarda141@gmail.com>
2026-06-07 17:47:48 -0400

    exercicio 10

--- lista.js
+++ lista.js
@@ -304,3 +304,51 @@ const carro = new Carro("Fiat", "Uno", 0, 30);
 const eletrico = new CarroEletrico("Tesla", "Model 3", 0, 100);
 carro.acelerar();
 eletrico.acelerar();
+
+class Forma {
+    calcularArea() {
+        throw new Error("Método deve ser implementado.");
+    }
+}
+
+class Circulo extends Forma {
+    constructor(raio) {
+        super();
+        this.raio = raio;
+    }
+
+    calcularArea() {
+        return Math.PI * this.raio ** 2;
+    }
+}
+
+class Retangulo extends Forma {
+    constructor(base, altura) {
+        super();
+        this.base = base;
+        this.altura = altura;
+    }
+
+    calcularArea() {
+        return this.base * this.altura;
+    }
+}
+
+class Triangulo extends Forma {
+    constructor(base, altura) {
+        super();
+        this.base = base;
+        this.altura = altura;
+    }
+
+    calcularArea() {
+        return (this.base * this.altura) / 2;
+    }
+}
+
+const circulo = new Circulo(5);
+const retangulo = new Retangulo(10, 4);
+const triangulo = new Triangulo(8, 6);
+console.log(circulo.calcularArea());
+console.log(retangulo.calcularArea());
+console.log(triangulo.calcularArea());
