28c89b441ed0aebed2cfcfcd5053ba622cde7f32
Dudetama <motaeduarda141@gmail.com>
2026-06-07 17:56:18 -0400

    exercicio 14

--- lista.js
+++ lista.js
@@ -480,3 +480,46 @@ const endereco = new Endereco("Rua das Flores", "Nova Andradina");
 const contato = new Contato("67999999999", "julia@email.com");
 const usuario = new PerfilUsuario("Julia", endereco, contato);
 usuario.exibirPerfil();
+
+class Aluno {
+    #nota1;
+    #nota2;
+
+    constructor(nome, nota1, nota2) {
+        this.nome = nome;
+        this.#nota1 = nota1;
+        this.#nota2 = nota2;
+    }
+
+    get nota1() {
+        return this.#nota1;
+    }
+
+    get nota2() {
+        return this.#nota2;
+    }
+
+    set nota1(valor) {
+        if (valor >= 0 && valor <= 10) {
+            this.#nota1 = valor;
+        }
+    }
+
+    set nota2(valor) {
+        if (valor >= 0 && valor <= 10) {
+            this.#nota2 = valor;
+        }
+    }
+
+    static calcularMedia(nota1, nota2) {
+        return (nota1 + nota2) / 2;
+    }
+}
+
+const aluno = new Aluno("Julia", 8, 9);
+console.log("Nota 1:", aluno.nota1);
+console.log("Nota 2:", aluno.nota2);
+console.log(
+    "Média:",
+    Aluno.calcularMedia(aluno.nota1, aluno.nota2)
+);
