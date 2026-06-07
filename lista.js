5f286add5d68675eb382c1a1389419fa874dd2c1
Dudetama <motaeduarda141@gmail.com>
2026-06-07 17:19:38 -0400

    exercicio 5

--- lista.js
+++ lista.js
@@ -97,3 +97,30 @@ const temp = new Temperatura(25);
 console.log(`Celsius: ${temp.celsius}`);
 console.log(`Fahrenheit: ${temp.fahrenheit}`);
 console.log(`Kelvin: ${temp.kelvin}`);
+
+class Usuario {
+    #senha;
+
+    constructor(nome, senha) {
+        this.nome = nome;
+        this.#senha = senha;
+    }
+
+    autenticar(senha) {
+        return senha == this.#senha;
+    }
+
+    alterarSenha(senhaAtual, novaSenha) {
+        if (senhaAtual == this.#senha) {
+            this.#senha = novaSenha;
+            console.log("Senha alterada com sucesso!");
+        } else {
+            console.log("Senha atual incorreta!");
+        }
+    }
+}
+
+const usuario = new Usuario("Julia", "1234");
+console.log(usuario.autenticar("1234"));
+usuario.alterarSenha("1234", "abcd");
+console.log(usuario.autenticar("abcd"));
