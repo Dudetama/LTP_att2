315d393b80e8b7960af28a6afc07556d8b53aee7
Dudetama <motaeduarda141@gmail.com>
2026-06-07 17:14:25 -0400

    exercicio 3

--- lista.js
+++ lista.js
@@ -42,3 +42,29 @@ class ContaBancaria {
         console.log("Saldo atual R$ " + this.saldo);
     }
 }
+
+class Produto {
+    static totalProdutos = 0;
+
+    constructor(nome, preco, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+
+        Produto.totalProdutos++;
+    }
+
+    calcularValorEstoque() {
+        return this.preco * this.quantidade;
+    }
+
+    static obterTotal() {
+        return Produto.totalProdutos;
+    }
+}
+
+const p1 = new Produto("Mouse", 50, 10);
+const p2 = new Produto("Teclado", 100, 5);
+
+console.log(p1.calcularValorEstoque());
+console.log(Produto.obterTotal());
