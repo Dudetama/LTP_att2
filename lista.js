c997c53d1c47d999c47ad558e1288504d0c2afa6
Dudetama <motaeduarda141@gmail.com>
2026-06-07 16:30:06 -0400

    exercicio 1

--- lista.js
+++ lista.js
@@ -1,1 +1,20 @@
-let o = 0
+class Pessoa {
+    constructor(nome, idade, email){
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    apresentar(){
+        console.log("Oi meu nome é "+ this.nome+ " e tenho "+ this.idade+ " anos");
+    }
+
+    fazerAniversario(){
+        this.idade++;
+    }
+}
+
+const pessoa = new Pessoa("Jolea", 12, "jubsjubs@gmail.com");
+pessoa.apresentar();
+pessoa.fazerAniversario();
+pessoa.apresentar();
